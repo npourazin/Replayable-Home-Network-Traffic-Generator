2a59f2b277a0bfc58f12eefa4c26e89f56f415c9
Niki Pourazin <nikipourzin@gmail.com>
2022-08-14 13:37:39 +0430

    Create ModelGenerator package

---
+++ ModelGenerator/LoadFlows.py
@@ -0,0 +1,15 @@
+
+class LoadFlows:
+    flow_list = []
+    # todo create a model using the detected flows
+
+    def __init__(self, filename):
+        self.load_flows(filename)
+
+    def load_flows(self, filename):
+        # todo: open the file with the recorded flows and load the flows in a list (:flow_list)
+        pass
+
+    def get_flows(self):
+        return self.flow_list
+
---
+++ ModelGenerator/ModelifyFlow.py
@@ -0,0 +1,9 @@
+
+
+class ModelFlow:
+    flow_list = []
+    # todo create a model using the detected flows
+
+    def model_a_flow(self, flow):
+        # todo: take a flow as input and create a model
+        pass
