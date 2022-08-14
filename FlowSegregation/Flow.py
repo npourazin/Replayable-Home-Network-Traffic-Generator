5f7dfb28d282ef145259f492fbda095f89080e18
Niki Pourazin <nikipourzin@gmail.com>
2022-08-14 10:35:35 +0430

    Use timestamps in flow durations

--- FlowSegregation/Flow.py
+++ FlowSegregation/Flow.py
@@ -20,22 +20,49 @@ class Flow:
         pass
 
     def calc_duration(self):
-        # todo: first pac ts to last pac ts
-        # first = get_first_flow_packet
-        # last = get_last_flow_packet
-
-        new_duration = 0
-        # calc it
+        new_duration = self.last_flow_packet.ts - self.first_flow_packet.ts
         self.duration = new_duration
 
     def create_record(self):
         # todo: save the flow as a record on disk
         pass
 
-    def add_new_pac(self, pac):
-        self.pacs.append(pac)
-        # todo: might need to check ts and update first and last ones
-        # todo: if at least one of first and last are None, we need to update (make update func maybe)
+    def update_with_new_pac(self, pac):
 
+        # we check different cases for  first_flow_packet and last_flow_packet
+
+        # first case: at least one of them is none
+        none_flag = False
+        if self.first_flow_packet is None:
+            self.first_flow_packet = pac
+            none_flag = True
+        if self.last_flow_packet is None:
+            self.last_flow_packet = pac
+            none_flag = True
+        if none_flag:
+            return
+
+        # second case: both are the same but different from this pac
+        if self.first_flow_packet == self.last_flow_packet and self.first_flow_packet != pac:
+            if pac.ts < self.first_flow_packet.ts:
+                self.first_flow_packet = pac
+                return
+            else:
+                self.last_flow_packet = pac
+                return
+
+        # third case: none of them is none, and the are different from each other
+        if pac.ts < self.first_flow_packet:
+            self.first_flow_packet = pac
+
+        elif pac.ts > self.last_flow_packet:
+            self.last_flow_packet = pac
+
+        # after all cases, recalculate the flow duration
         self.calc_duration()
 
+    def add_new_pac(self, pac):
+        self.pacs.append(pac)
+        self.update_with_new_pac(pac)
+
+        self.calc_duration()
